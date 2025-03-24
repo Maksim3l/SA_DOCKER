e4c4777ac88531db1ba71082bc699f581eb6ec26
Maksim3l <loknarmaksim@gmail.com>
2025-03-24 17:15:22 +0100

    ADD index.html
    Added index.html and templates folder to use render_template and open the flask client on port 8000

--- Client/app/app.py
+++ Client/app/app.py
@@ -1,2 +1,12 @@
 import requests
 from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def image_page():
+    return render_template('index.html')
+    
+if __name__ == '__main__':
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(host='0.0.0.0', port=8000, debug=True)
