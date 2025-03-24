402ed738afc8214b474dee55e8837cfbeed3bd2c
Maksim3l <loknarmaksim@gmail.com>
2025-03-24 17:33:06 +0100

    UPDATE server app.py again
    Added the bare bones get request, connection to client works

--- Server/app/app.py
+++ Server/app/app.py
@@ -6,5 +6,19 @@ import time
 
 app = Flask(__name__)
 
+@app.route('/', methods=["GET"])
+def get_img():
+
+    cap = cv2.VideoCapture(0)
+
+    ret, frame = cap.read()
+
+    _, buffer = cv2.imencode('.jpg', frame)
+    img_base64 = base64.b64encode(buffer).decode('utf-8')
+
+    cap.release()
+
+    return jsonify({'img': img_base64})
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
